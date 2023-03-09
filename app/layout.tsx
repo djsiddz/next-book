bba401a7fde2d3626d0fb2c4d239a76ea9a4bd8e
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-03-09 18:47:39 +0530

    Remove old CSS; Add React Bootstrap with types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import './globals.css'
-
 export const metadata = {
   title: 'Next Book',
   description: 'What are you reading?',
@@ -12,6 +10,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="container">
       <h1>Next Book</h1>
     </main>
   )
