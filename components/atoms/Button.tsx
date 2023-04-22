0754bb07ac88c7161df8bf0f11b7c1cb9b853baa
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-04-22 23:40:11 +0530

    Remove unnecessary React import

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import React from 'react';
 import { ButtonProps } from './ButtonProps';
+
+import Link from 'next/link';
 
 export function Button({ text, href }: ButtonProps) {
   return (
