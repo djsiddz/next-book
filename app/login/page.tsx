763784bb29a0fadb9ec4ec2937c22db853b14b8a
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-04-22 17:29:57 +0530

    Explicit set showNavigation for Header in landing and login pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <>
       <div className="p-10">
-        <Header />
+        <Header showNavigation={false} />
         <main className="flex h-screen flex-col justify-center">
           <h1 className="mb-6 text-6xl">Hello there!</h1>
           <p className="text-md mb-6">Sign in to get started!</p>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Header } from '@/templates/Header';
 export default function Home() {
   return (
     <div className="p-10">
-      <Header />
+      <Header showNavigation={false} />
       <main className="flex h-screen flex-col justify-center">
         <h1 className="mb-6 text-6xl">Keep track of your Books.</h1>
         <p className="mb-12 text-xl">No matter where and how you like to read them.</p>
