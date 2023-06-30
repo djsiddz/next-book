08ecd8429442eb92239442dd45769ef03fe7c3a7
Siddhesh Thadeshwar <siddhesht@outlook.in>
2023-06-30 21:54:37 +0530

    fix(app): Add/remove React imports as actually required

--- components/templates/Header.tsx
+++ components/templates/Header.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 type HeaderProps = {
@@ -31,7 +32,7 @@ export function Header({ showNavigation = false }: HeaderProps) {
             <li className="ml-auto">
               <Link href="/settings" className="text-md">
                 <span className="pr-3">Hello User</span>
-                <img
+                <Image
                   className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
                   src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt="User Profile"
