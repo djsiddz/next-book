24b3c8fb41b1bc48808fa5f12ba80127a70cee82
Siddhesh Thadeshwar <siddhesht@outlook.in>
2024-01-12 18:51:05 +0530

    feat(app): Add NavigationMenu component from shadcn UI; Test implement
    - Implementing Navigation Menu was giving errors, which was resolved by adding 'use client' to the top of the page where Navigation Menu is included.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,36 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
+import { ChevronRightSquareIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="p-10">
+      <NavigationMenu className="w-100">
+        <NavigationMenuList>
+          <NavigationMenuItem className="flex items-center">
+            <ChevronRightSquareIcon size={36} className="inline" /> <span className="pl-3">Next Book</span>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
       <main className="flex h-screen flex-col justify-center">
         <h1 className="mb-6 text-6xl">Keep track of your Books.</h1>
         <p className="mb-12 text-xl">No matter where and how you like to read them.</p>
-        <Button asChild>
-          <Link href="/login">Get Started</Link>
-        </Button>
-        <Button variant="secondary">Learn More</Button>
+        <div className="flex justify-start gap-4">
+          <Button asChild className="w-min text-2xl px-10 py-8">
+            <Link href="/login">Get Started</Link>
+          </Button>
+          <Button variant="secondary" className="w-min text-2xl px-10 py-8">Learn More</Button>
+        </div>
       </main>
     </div>
   );
