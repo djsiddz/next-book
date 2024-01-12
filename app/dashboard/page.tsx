7164d9dd93eb23ce7c188d9590ebe615a080e5ae
Siddhesh Thadeshwar <siddhesht@outlook.in>
2024-01-12 16:39:23 +0530

    refactor(app): Remove previous code to start fresh

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <>
-      <div className="w-3/4">Big Sections</div>
-      <aside className="w-1/4">Smaller cards</aside>
-    </>
-  );
-}
--- app/settings/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <>
-      <div className="w-3/4">User Profile</div>
-      <aside className="w-1/4">Change Password</aside>
-    </>
-  );
-}
