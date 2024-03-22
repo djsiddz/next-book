6a0e89760ba935fa5557326fc2ecee5fd8f1d00c
Siddhesh Thadeshwar <siddhesht@outlook.in>
2024-03-22 11:28:23 +0530

    enhancements(app): Update error page UI

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,13 +1,17 @@
 export default function ErrorPage() {
   return (
-    <div className="p-10 flex flex-col justify-center items-center min-h-screen">
+    <div className="flex min-h-screen flex-col items-center justify-center p-10">
       <p className="font-bold text-yellow-400">Something went wrong!</p>
       <p className="">It's not you. It's us. Come back in a while and try again.</p>
-      <pre className="mt-32">🎵 I'm having a bad bad day
-        <br />It's about time that I get my way
-        <br />Steam rolling whatever I see, huh
-        <br />Despicable me 🎵
+      <pre className="mt-32">
+        🎵 I'm having a bad bad day
+        <br />
+        It's about time that I get my way
+        <br />
+        Steam rolling whatever I see, huh
+        <br />
+        Despicable me 🎵
       </pre>
     </div>
-  )
+  );
 }
