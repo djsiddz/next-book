14d52056a5e013a90d97657b7e07f3d57057f088
Siddhesh Thadeshwar <siddhesht@outlook.in>
2024-09-02 17:45:25 +0530

    test(app): Update tests for Homepage

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,19 +1,34 @@
 import Home from "../app/page";
 
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-describe('Home page', () => {
-  // This is the AAA pattern for testing.
-  it('should have "Keep track of your Books"', () => {
-    render(<Home />) // ARRANGE
-    const headingElement = screen.getByText("Keep track of all your Books.");
-    expect(headingElement).toBeInTheDocument(); // ASSERT
-  })
-  // Adding one more test.
-  it('should have "Join the Waitlist" button', () => {
-    render(<Home />) // ARRANGE
-    const ctaElement = screen.getByText("Join the Waitlist"); // ACT
-    expect(ctaElement).toBeInTheDocument(); // ASSERT
-  })
+// Import { BrowserRouter as Router } from 'react-router-dom';
 
-})
+describe('Home Component', () => {
+  test('renders the Home component', () => {
+    render(
+      // <Router>
+        <Home />
+      // </Router>
+    );
+
+    // Check for Nav component
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+
+    // Check for main headings
+    expect(screen.getByText('Keep track of all your Books.')).toBeInTheDocument();
+    expect(screen.getByText('And your reading habit. No matter where and how you like to read them.')).toBeInTheDocument();
+
+    // Check for buttons
+    expect(screen.getByRole('link', { name: /Join the Waitlist/i })).toHaveAttribute('href', '/signup?campaign=Direct');
+    expect(screen.getByRole('button', { name: /Learn More/i })).toBeInTheDocument();
+
+    // Check for features section
+    expect(screen.getAllByText('Features')).toHaveLength(2);
+    expect(screen.getByText('All Books in one view')).toBeInTheDocument();
+    expect(screen.getByText('Track your progress')).toBeInTheDocument();
+    expect(screen.getByText('Build a reading habit')).toBeInTheDocument();
+    expect(screen.getByText('Get AI recommendations')).toBeInTheDocument();
+ });
+});
