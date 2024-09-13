4a24f54a140fd247c2510b39fed4adb2f821115d
Siddhesh Thadeshwar <siddhesht@outlook.in>
2024-09-13 21:31:18 +0530

    fix(app): Fix duplicate keys on Feature icons

--- components/native/FeatureCardGrid.tsx
+++ components/native/FeatureCardGrid.tsx
@@ -7,9 +7,9 @@ export default function FeatureCardGrid() {
   // TODO: Find a better way to handle icons?
   const iconSet = [
     <BoxesIcon key={"boxIcon"} size={48} className="text-yellow-500" />,
-    <BookCheckIcon key={"boxIcon"} size={48} className="text-yellow-500" />,
-    <Tally5Icon key={"boxIcon"} size={48} className="text-yellow-500" />,
-    <SparklesIcon key={"boxIcon"} size={48} className="text-yellow-500" />,
+    <BookCheckIcon key={"bookCheckIcon"} size={48} className="text-yellow-500" />,
+    <Tally5Icon key={"tally5Icon"} size={48} className="text-yellow-500" />,
+    <SparklesIcon key={"sparklesIcon"} size={48} className="text-yellow-500" />,
   ];
   return (
     <div className="mt-16 flex flex-col gap-4 md:grid md:grid-cols-2">
